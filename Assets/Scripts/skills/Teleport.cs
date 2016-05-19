d949227be53b82b4597701b226037477b324ab47
David-Caminha <david.caminha94@gmail.com>
2016-05-19 20:01:55 +0100

    Multiplayer no silence

---
+++ Assets/Scripts/skills/Teleport.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq.Expressions;
+
+public class Teleport : MonoBehaviour
+{
+
+    private GameObject[] teamMates;
+    private int leastHealth = 100000;
+    private int teamMateHealth = 0;
+    private GameObject playerObjective = null;
+    private Vector3 positionPlayerObjective;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            //Search which teamMate has the least health and save it on playerObjective
+            teamMates = GameObject.FindGameObjectsWithTag("teammate");
+            foreach (GameObject teamMate in teamMates)
+            {
+                teamMateHealth = teamMate.GetComponent<PlayerStats>().Health;
+                print("If" + teamMateHealth);
+                if (teamMateHealth != 0 && teamMateHealth < leastHealth)
+                {
+                    
+                    leastHealth = teamMateHealth;
+                    playerObjective = teamMate;
+                }
+            }
+            positionPlayerObjective = playerObjective.transform.position;
+
+            //ERROR de entrar nas paredes
+            transform.position = positionPlayerObjective + new Vector3(0, 0, 3);
+        }
+    }
+}
