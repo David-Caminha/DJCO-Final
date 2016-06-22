f82d211a228973d41bc123c90333f8ea329cf5bf
David-Caminha <david.caminha94@gmail.com>
2016-06-22 10:54:45 +0100

    Para gravaçao

--- Assets/Scripts/Objects/MovingPlataforms.cs
+++ Assets/Scripts/Objects/MovingPlataforms.cs
@@ -9,33 +9,35 @@ public class MovingPlataforms : MonoBehaviour {
     public float time;
     public float velocity;
     public float coldown;
+    public Vector3 pos;
     // Use this for initialization
     void Start () {
-	    
+        pos = up.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if(movingUp)
         {
-            transform.position = Vector3.Lerp(transform.position, up.position, velocity * time);
+            transform.position = Vector3.Lerp(transform.position, pos, velocity * time);
             time += Time.deltaTime;
             if (time >= coldown)
             {
                 movingUp = false;
                 time = 0;
-
+                pos = down.position;
             }
             return;
         }
             else
         {
-            transform.position = Vector3.Lerp(transform.position, down.position, velocity * time);
+            transform.position = Vector3.Lerp(transform.position, pos, velocity * time);
             time += Time.deltaTime;
             if (time >= coldown)
             {
                 movingUp = true;
                 time = 0;
+                pos = up.position;
 
             }
             return;
